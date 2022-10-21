9ef75e15ada389046d3f2319a73ab716b1bd59c9
Alex Kelly <kellya@arachnitech.com>
2022-10-20 22:06:11 -0400

    Prepare for a db-backed configuration
    - Plumb in the basics for a db storage backend
    - move to a subcomman-based interaction (for setting prefs and keeping
    everying a little more tidy)
    - modularize things into functions
    - add a wx alias

--- weather/weather.py
+++ weather/weather.py
@@ -1,10 +1,36 @@
 """ maubot to get the weather from wttr.in and post in matrix chat """
 
+from __future__ import annotations
 from typing import Type
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+from mautrix.util.async_db import UpgradeTable, Connection
 from yarl import URL
+
+upgrade_table = UpgradeTable()
+
+
+@upgrade_table.register(description="Initial revision")
+async def upgrade_v1(conn: Connection) -> None:
+    await conn.execute(
+        """CREATE TABLE wx_location_pref (
+            username   TEXT PRIMARY KEY,
+            city TEXT NOT NULL
+        )"""
+    )
+
+
+@upgrade_table.register(description="Remember user who added value")
+async def upgrade_v2(conn: Connection) -> None:
+    await conn.execute("ALTER TABLE wx_location_pref ADD COLUMN creator TEXT")
+
+
+@upgrade_table.register(description="Remember user who added value")
+async def upgrade_v3(conn: Connection) -> None:
+    await conn.execute(
+        "insert into wx_location_pref values('default','Columbus, OH','')"
+    )
 
 
 class Config(BaseProxyConfig):
@@ -25,6 +51,10 @@ class WeatherBot(Plugin):
         self.config.load_and_update()
 
     @classmethod
+    def get_db_upgrade_table(cls) -> UpgradeTable | None:
+        return upgrade_table
+
+    @classmethod
     def get_config_class(cls) -> Type[BaseProxyConfig]:
         return Config
 
@@ -34,41 +64,10 @@ class WeatherBot(Plugin):
             location = self.config["default_location"]
         location = location.replace(", ", "_")
         location = location.replace(" ", "+")
-        return location
+        return location.strip()
 
-    @command.new(name="weather", help="Get the weather")
-    @command.argument("location", pass_raw=True)
-    async def weather_handler(self, evt: MessageEvent, location=None) -> None:
-        """Listens for !weather and returns a message with the result of
-        a call to wttr.in for the location specified by !weather <location>
-        or by the config file if no location is given"""
-        if location and location == "help":
-            await evt.respond(
-                """
-                Uses wttr.in to get the weather and respond. If you
-                don't specify a location, it will use the IP address
-                of the server to figure out what the location is.
-
-                Otherwise, you may specify the location by name:
-                !weather Chicago
-
-                or by Airport Code
-                !weather SFO
-
-                The units may be specified with a location by adding
-                u:<unit> to the end of the location like:
-                !weather Chicago u:m
-
-                Where <unit> is one of:
-                m = metric
-                u = US
-                M = metric, but wind in m/s
-                """
-            )
-        units = ""  # default to nothing so that response works even if default is unset
-
+    async def get_weather(self, location, units=None) -> str:
         if self.config["default_units"]:
-            # If units are specified in the config, use them
             units = f"&{self.config['default_units']}"
         if "u:" in location:
             # If the location has units specified, attempt to use them
@@ -91,6 +90,26 @@ class WeatherBot(Plugin):
                 "This issue will probably resolve itself, so sit "
                 "tight and look out the window until it does"
             )
+        return message
+
+    @command.new(
+        name="weather",
+        aliases=("wx",),
+        help="Get the weather",
+        require_subcommand=False,
+    )
+    async def weather(self, evt: MessageEvent) -> None:
+        message = await self.get_weather("Columbus,Ohio")
+        await evt.respond(message)
+
+    @weather.subcommand(name="get")
+    @command.argument(name="location", pass_raw=True)
+    async def wx_location(self, evt: MessageEvent, location="Columbus,oh") -> None:
+        """Listens for !weather and returns a message with the result of
+        a call to wttr.in for the location specified by !weather <location>
+        or by the config file if no location is given"""
+        units = ""  # default to nothing so that response works even if default is unset
+        message = await self.get_weather(location)
         await evt.respond(message)
         if self.config["show_image"]:
             wttr_url = "http://wttr.in"
@@ -110,10 +129,46 @@ class WeatherBot(Plugin):
             else:
                 await evt.respond(f"error getting location {location}")
 
+    @weather.subcommand(name="help", help="show help")
+    async def wx_tips(self, evt: MessageEvent):
+        await evt.respond(
+            """
+                Uses wttr.in to get the weather and respond. If you
+                don't specify a location, it will use the IP address
+                of the server to figure out what the location is.
+
+                Otherwise, you may specify the location by name:
+                !weather Chicago
+
+                or by Airport Code
+                !weather SFO
+
+                The units may be specified with a location by adding
+                u:<unit> to the end of the location like:
+                !weather Chicago u:m
+
+                Where <unit> is one of:
+                m = metric
+                u = US
+                M = metric, but wind in m/s
+                """
+        )
+
+    @command.new(name="wxpref", help="set weather bot preferences")
+    @command.argument("preference", pass_raw=True)
+    async def set_prefs(self, evt: MessageEvent, preference):
+        """sets preferences in database"""
+        q = """
+            INSERT INTO wx_location_pref (username, city) VALUES ($1,$2)
+            ON CONFLICT (username) DO UPDATE SET username=excluded.username, city=excluded.city
+        """
+        await self.database.execute(q, evt.sender, preference)
+        await evt.respond(f"Inserted {preference} as your default location")
+
     @command.new(name="moon", help="Get the moon phase")
     async def moon_phase_handler(
-        self,
-        evt: MessageEvent,
+            self,
+            evt: MessageEvent,
     ) -> None:
         """Get the lunar phase from wttr.in json and respond in chat"""
         # Associate the utf-8 character with the name of the phase
